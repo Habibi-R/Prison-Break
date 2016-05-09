0ef7fbf1b71e63cd9bcc44b5b22e0f691b2d68c5
Habibi-R <rhabibi@bayviewglen.ca>
2016-05-09 15:03:19 -0400

    initial test commit
    test commit

---
+++ src/Main/Play.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class Play {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Game playGame = new Game(); 
+		
+		playGame.play();
+		
+		
+		
+	}
+
+}
