5b52604d2ad813f3e36e5885fc7cd74727c2f8a1
Habibi-R <rhabibi@bayviewglen.ca>
2016-05-19 15:10:29 -0400

    Added inventory and item classes, WiP

---
+++ src/com/bayviewglen/prsnbrk/Inventory.java
@@ -0,0 +1,11 @@
+package com.bayviewglen.prsnbrk;
+
+import java.util.ArrayList;
+
+public class Inventory {
+
+	public Inventory(){
+		ArrayList<Item> inv = new ArrayList<Item>();
+	}
+	
+}
---
+++ src/com/bayviewglen/prsnbrk/Item.java
@@ -0,0 +1,32 @@
+package com.bayviewglen.prsnbrk;
+
+public class Item {
+
+	private String name = "";
+	private String description = "";
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setName(String nameToSet){
+			
+			name = nameToSet;
+	}
+	
+	public void setDescription(String descriptionToSet){
+		
+			description = descriptionToSet;
+	}
+	
+	
+
+	
+
+}
