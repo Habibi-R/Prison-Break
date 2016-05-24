3d1a58df65ea0acab014e6c2afc516ab0b456879
Aadithya Shankar <shankar001@icloud.com>
2016-05-24 09:37:24 -0400

    ctrl-shift f

--- src/com/bayviewglen/prsnbrk/Command.java
+++ src/com/bayviewglen/prsnbrk/Command.java
@@ -3,65 +3,58 @@ package com.bayviewglen.prsnbrk;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-class Command implements Serializable
-{
-	
+class Command implements Serializable {
+
 	private String commandWord;
-    private String secondWord;
-    private String thirdWord;
+	private String secondWord;
+	private String thirdWord;
 
-    /**
-     * Create a command object. First and second word must be supplied, but
-     * either one (or both) can be null. The command word should be null to
-     * indicate that this was a command that is not recognized by this game.
-     */
-    public Command(String firstWord, String secondWord, String thirdWord)
-    {
-        commandWord = firstWord;
-        this.secondWord = secondWord;
-        this.thirdWord = thirdWord;
-    }
+	/**
+	 * Create a command object. First and second word must be supplied, but
+	 * either one (or both) can be null. The command word should be null to
+	 * indicate that this was a command that is not recognized by this game.
+	 */
+	public Command(String firstWord, String secondWord, String thirdWord) {
+		commandWord = firstWord;
+		this.secondWord = secondWord;
+		this.thirdWord = thirdWord;
+	}
 
-    /**
-     * Return the command word (the first word) of this command. If the
-     * command was not understood, the result is null.
-     */
-    public String getCommandWord()
-    {
-        return commandWord;
-    }
+	/**
+	 * Return the command word (the first word) of this command. If the command
+	 * was not understood, the result is null.
+	 */
+	public String getCommandWord() {
+		return commandWord;
+	}
 
-    /**
-     * Return the second word of this command. Returns null if there was no
-     * second word.
-     */
-    public String getSecondWord()
-    {
-        return secondWord;
-    }
+	/**
+	 * Return the second word of this command. Returns null if there was no
+	 * second word.
+	 */
+	public String getSecondWord() {
+		return secondWord;
+	}
 
-    public String getThirdWord()
-    {
-    	return thirdWord;
-    }
-    /**
-     * Return true if this command was not understood.
-     */
-    public boolean isUnknown()
-    {
-        return (commandWord == null);
-    }
+	public String getThirdWord() {
+		return thirdWord;
+	}
 
-    /**
-     * Return true if the command has a second word.
-     */
-    public boolean hasSecondWord()
-    {
-        return (secondWord != null);
-    }
-    
-    public boolean hasThirdWord()
-    {
-        return (thirdWord != null);
-    }
+	/**
+	 * Return true if this command was not understood.
+	 */
+	public boolean isUnknown() {
+		return (commandWord == null);
+	}
+
+	/**
+	 * Return true if the command has a second word.
+	 */
+	public boolean hasSecondWord() {
+		return (secondWord != null);
+	}
+
+	public boolean hasThirdWord() {
+		return (thirdWord != null);
+	}
 }
